706a3a8a2402957a73864ccd6372878acf9755d1
pinomaker-hoo <inhoo23@naver.com>
2023-06-23 15:36:33 +0900

    feat : Set Mui Config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 // ** React Imports
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 // ** Style Imports
 import './style/global.css'
+import { ThemeProvider } from '@mui/material'
+import theme from 'theme'
+import { GlobalStyles } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline'
 
 // ** Component Imports
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles styles={{ backgroundColor: 'blue' }} />
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+)
