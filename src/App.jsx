1657bd3b89483c008029bc7646ddd42b95abbc2c
pinomaker-hoo <inhoo23@naver.com>
2023-06-27 17:07:31 +0900

    feat : DatePicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import DatePicker from 'components/date-picker'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import dayjs from 'dayjs'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Hello world</h1>} />
+        <Route
+          path="/"
+          element={<DatePicker value={dayjs()} handleChange={() => null} />}
+        />
       </Routes>
     </BrowserRouter>
   )
